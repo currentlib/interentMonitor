83d3944597d14c50c0852caeb6c73687ad463114
currentlib <y.tymkiv.official@gmail.com>
2021-02-17 04:47:46 +0200

    sad

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.send("Hello from GETasdsadsadsa");
+    res.sendStatus(200);
 });
 
 app.post('/', (req, res) => {
-    res.send("Hello from POSTsadsaddasdsad");
+    console.log(req.body);
+
+    res.sendStatus(200);
+    inputParser(req.body);
 })
 
 app.listen(3000);
+
+
+function inputParser(input) {
+    switch (input.type) {
+        case "linux":
+            console.log("LINUX");
+            parseLinux(input.payload)
+            return 0;
+        case "windows":
+            console.log("WINDOWS");
+            return 0;
+    };
+}
+
+function parseLinux(toParse) {
+    let parseArr = toParse.split(";")
+    console.log(parseArr[2] + " | " + parseArr[3]);
+}
